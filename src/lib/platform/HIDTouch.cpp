39660144acea24fd1179cbdbd946482e4d9fb33f
Austin Lawrence <austin.lawrence@gmail.com>
2022-08-24 12:12:42 -0700

    x

--- src/lib/platform/HIDTouch.cpp
+++ src/lib/platform/HIDTouch.cpp
@@ -20,8 +20,8 @@ void HIDTouch::move(float fx, float fy) {
 
     m_data[0] = 0x02;
 
-    auto xCoord = (UInt32) (fx * RESOLUTION);
-    auto yCoord = (UInt32) (fy * RESOLUTION);
+    auto x = (UInt32) (fx * RESOLUTION);
+    auto y = (UInt32) (fy * RESOLUTION);
 
     LOG((CLOG_DEBUG "%u %u", x, y));
 
@@ -35,25 +35,3 @@ void HIDTouch::move(float fx, float fy) {
 
     update();
 }
-
-/*
-void HIDTouch::move(float xFrac, float yFrac) {
-
-    m_data[0] = 0x02;
-
-    auto xCoord = (UInt32) (xFrac * RESOLUTION);
-    auto yCoord = (UInt32) (yFrac * RESOLUTION);
-
-    LOG((CLOG_DEBUG "%u %u", xCoord, yCoord));
-
-    m_data[1] = xCoord & 255;
-    m_data[2] = (xCoord >> 8) & 255;
-
-    m_data[3] = yCoord & 255;
-    m_data[4] = (yCoord >> 8) & 255;
-
-    LOG((CLOG_DEBUG "%u %u, %u %u", m_data[1], m_data[2], m_data[3], m_data[4]));
-
-    update();
-}
-*/

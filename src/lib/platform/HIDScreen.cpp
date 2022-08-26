51e9c33e7c7a16f000c7bce92a2d6d9202760dd1
Austin Lawrence <austin.lawrence@gmail.com>
2022-08-26 09:37:13 -0700

    x

--- src/lib/platform/HIDScreen.cpp
+++ src/lib/platform/HIDScreen.cpp
@@ -172,7 +172,7 @@ void HIDScreen::enter()
     // Move the mouse to a known location
     m_mouseX = 0;
     m_mouseY = 0;
-    m_mouseAbsDevice.move(0, 0);
+    m_mouseAbsDevice.move(0.1, 0.1);
 }
 
 bool HIDScreen::leave()
